37a6d53c0504c80ea120694b1fad2e71cc18df23
April Copes <antdevelopment1@gmail.com>
2020-04-16 01:06:31 -0400

    Creates react app

---
+++ server.js
@@ -0,0 +1,17 @@
+// const express = require('express');
+// const app = express();
+// app.use(express.static(__dirname+'/'));
+// app.listen(3000);
+// console.log("Node/Express server is running on port 3000");
+
+
+
+
+
+
+
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(3000);
+console.log("Node is listening on port 3000");
